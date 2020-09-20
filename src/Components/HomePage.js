6ea985ce55b2863ddb4be61125ecd59ce826278e
jacob <jacobcunningham96@yahoo.com>
2020-09-20 10:40:41 -0600

    finished initial setup of home page and wired up apollo client

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,9 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
-      <h2>HomePage</h2>
+    <div
+      style={{
+        backgroundColor: "#0d2f81",
+        height: "95vh",
+        width: "100%",
+        display: "gird",
+      }}
+    >
+      <h2
+        style={{
+          color: "#ffe7aa",
+          fontFamily: "'Atlas Grotesk Web', 'Open Sans', sans-serif;",
+          fontSize: "2rem",
+          placeSelf: "center stretch",
+          paddingTop: "2rem",
+          paddingLeft: "2rem",
+        }}
+      >
+        Lyriql Gangsta
+      </h2>
+      <div style={{ display: "flex" }}>
+        <h3
+          style={{
+            color: "#ffe7aa",
+            fontFamily: "'Atlas Grotesk Web', 'Open Sans', sans-serif;",
+            fontSize: "3.5rem",
+            width: "35%",
+            paddingLeft: "10rem",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Build songs and have friends collaborate on the lyrics
+        </h3>
+        <img src="/"></img>
+      </div>
+      <div style={{ float: "right", paddingRight: "35rem" }}>
+        <Link to="/songs">
+          <button
+            className="ui button"
+            style={{
+              backgroundColor: "#fff",
+              color: "#000",
+              fontSize: "1.5rem",
+            }}
+          >
+            Let's make some music!
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "react-apollo";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const client = new ApolloClient({
+  uri: "https://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.querySelector("#root")
+);
